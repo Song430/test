fe2a6823449052ef40dc4b8fd75ba480680db516
Song430 <18813096445@163.com>
2020-08-21 20:37:21 +0800

    a new file to learn argparse

--- test_python.py
+++ test_python.py
@@ -1,4 +1,10 @@
 # to learn how to use git
-print("I am a monkey")
-print("I do")
-print("Hello")
+import argparse
+parser = argparse.ArgumentParser(description="Process some intergers")
+parser.add_argument("integers",metavar="N",type=int, nargs="+",
+        help="an inter for the accumulator")
+parser.add_argument("--sum", dest="accumulate", action="store_const",
+        const = sum, default=max,
+        help="sum the integers(default: find the max)")
+args = parser.parse_args()
+print(args.accumulate(args.integers))
