e9d3d495290330d10b0ea88fd3507672d3b3e0ee
Song430 <18813096445@163.com>
2020-10-22 16:29:03 +0800

    conflict fixed

--- test_python.py
+++ test_python.py
@@ -1,4 +1,5 @@
 # to learn how to use git
+# the second commit
 import argparse
 parser = argparse.ArgumentParser(description="Process some intergers")
 parser.add_argument("integers",metavar="N",type=int, nargs="+",
@@ -8,3 +9,6 @@ parser.add_argument("--sum", dest="accumulate", action="store_const",
         help="sum the integers(default: find the max)")
 args = parser.parse_args()
 print(args.accumulate(args.integers))
+print("I am a monkey")
+print("I do")
+print("Hello")
